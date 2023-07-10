6cec4a97f37ba14409a061dde53992f9772eefed
nabeelnashid <53267839+Nabeel-Nashid@users.noreply.github.com>
2023-07-10 16:42:41 +0530

    blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import styles from '../page.module.css'
+import styles from './page.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+import bg from '../../../public/pexels.jpeg'
 
 function Blog() {
   return (
-    <div className={styles.main}>
-      Blog
+    <div className={styles.mainContainer}>
+      
+        <Link href={`#`} className={styles.container}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={bg}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Title</h1>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </Link>
     </div>
   )
 }
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import styles from './page.module.css'
+import Button from "@/components/Button/Button";
+import Image from "next/image";
+import background from "../../../../public/apps.jpg"
 
-function Category() {
+function Category(params) {
   return (
-    <div>
-      Category
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} src={background} alt="" />
+        </div>
+      </div>
+
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} src={background} alt="" />
+        </div>
+      </div>
+
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} src={background} alt="" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
