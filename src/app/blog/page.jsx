4528b46df923a3733ddfc3c52209b77d1bac189d
nabeelnashid <53267839+Nabeel-Nashid@users.noreply.github.com>
2023-07-10 15:00:54 +0530

    first commit

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../page.module.css'
+
+function Blog() {
+  return (
+    <div className={styles.main}>
+      Blog
+    </div>
+  )
+}
+
+export default Blog
